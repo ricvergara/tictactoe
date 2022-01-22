56048ebe4e533b48a149567c44145e679e1d5855
ricvergara <ricardovergar@gmail.com>
2022-01-21 21:46:44 -0500

    Was added a modal and restart the game when someone win

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
 const playText = document.getElementById('playText');
+const winner = document.getElementById('winner');
 const restartBtn = document.getElementById('restartBtn')
 const spaces = [null, null, null, null, null, null, null, null, null];
 const O_TEXT = 'O';
@@ -27,6 +28,17 @@ const drawBoard = () => {
     })
 }
 
+const restar = () => {
+    spaces.forEach((space, index) => {
+        spaces[index] = null;
+    });
+    boxes.forEach((box) => {
+        box.innerText = '';
+    });
+    playText.innerText = "Let's play";
+    currentPlayer = O_TEXT;
+}
+
 const boxClicked = (e) => {
     const id = e.target.id;
     if (!spaces[id]) {
@@ -41,48 +53,91 @@ const boxClicked = (e) => {
     }
 }
 
+//modal
+        
+var modal = document.getElementById("showModal");
+        
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+const showWinner = function() {
+  modal.style.display = "block";
+  winner.innerText = `${currentPlayer} has won :D`;
+  restar();
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 const playerHasWon = () => {
     if (spaces[0] === currentPlayer) {
         if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
             console.log(`${currentPlayer} wins up top`);
+            showWinner();
             return true;
+
         }
         if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
             console.log(`${currentPlayer} wins on the left`);
+            showWinner();
             return true;
+
         }
         if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
             console.log(`${currentPlayer} wins diagonally`);
+            showWinner()
             return true;
         }
     }
     if (spaces[8] === currentPlayer) {
         if (spaces[6] === currentPlayer && spaces[7] === currentPlayer) {
             console.log(`${currentPlayer} wins up bottom`);
+            showWinner();
             return true;
+
         }
         if (spaces[2] === currentPlayer && spaces[5] === currentPlayer) {
             console.log(`${currentPlayer} wins on the right`);
+            showWinner();
             return true;
+
         }
     }
     if (spaces[6] === currentPlayer) {
-        if (spaces[2] === currentPlayer && spaces[4] === currentPlayer) {
+                if (spaces[2] === currentPlayer && spaces[4] === currentPlayer) {
             console.log(`${currentPlayer} wins diagonally`);
+            showWinner();
             return true;
+
         }
     }
     if (spaces[4] === currentPlayer) {
         if (spaces[1] === currentPlayer && spaces[7] === currentPlayer) {
             console.log(`${currentPlayer} wins vertically in the middle`);
+            showWinner();
             return true;
+
         }
         if (spaces[3] === currentPlayer && spaces[5] === currentPlayer) {
             console.log(`${currentPlayer} wins horizontally in the middle`);
+            showWinner();
             return true;
+
         }
     }
 }
+
 
 restartBtn.addEventListener("click", () => {
     spaces.forEach((space, index) => {
@@ -94,6 +149,8 @@ restartBtn.addEventListener("click", () => {
     playText.innerText = "Let's play";
     currentPlayer = O_TEXT;
 });
+
+
 
 drawBoard();
 
